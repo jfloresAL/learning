b4f8d000c596ea9dd5c437cb51eab6a602aa6a09
Jose Andres Flores Perez <jfloresperez13@gmail.com>
2026-04-16 17:16:48 -0500

    Update comparison message in ejemplo.py

--- ejemplo.py
+++ ejemplo.py
@@ -4,8 +4,9 @@ b=8
 if a==b:
     print("iguales")  
 else:    
-    print("gaaaaaaaaaaaaaaaaaaa")
+    print("son diferentes")
     
 print("fin del programa")
 
 print("cambio subido desde mi local")
+print("modifico desde la nuber")
