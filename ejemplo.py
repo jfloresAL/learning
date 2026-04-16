11fdb909c5aaf8588d4b632f8fbb74619b401e4a
Andres Flores <jfloresperez13@gmail.com>
2026-04-16 18:21:49 -0500

    cambios desde cero suelta

--- ejemplo.py
+++ ejemplo.py
@@ -3,12 +3,11 @@ b=191
 c=12;
 
 if a==b==c:
-    print("igualesdasds")  
+    print("cambio que realice despues de descargar tus cambios y ahora quiero subirliss")  
 else:    
     print("uno de los 3 numeros son diferentes")
     
 print("ahi vamos de nuevo")
 
 print("solo se que nada se ")
-print("modifico desde la nuber")
-print("agregue el c")
+
