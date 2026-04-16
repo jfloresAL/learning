674b089cbdb17522604086583dbe3ac85b0fe4e6
Andres Flores <jfloresperez13@gmail.com>
2026-04-16 18:36:58 -0500

    cambio des fech

--- ejemplo.py
+++ ejemplo.py
@@ -1,9 +1,9 @@
 a=5
 b=191
-c=12;
+c=12
 
 if a==b==c:
-    print("cambio que realice despues de descargar tus cambios y ahora quiero subirliss")  
+    print("cambio con fechs y despues pull")  
 else:    
     print("uno de los 3 numeros son diferentes")
     
