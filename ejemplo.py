f3210fe2987f5b60aae25f616ebf55bd963f850b
Andres Flores <jfloresperez13@gmail.com>
2026-04-20 11:27:18 -0500

    cambio en el eejmplo que realice en mi local del ejercicio1

--- ejemplo.py
+++ ejemplo.py
@@ -1,14 +1,32 @@
-a=5
-b=191
-c=12;
-b=1255
+print("=== CALCULADORA ===")
+print("1. Sumar")
+print("2. Restar")
+print("3. Multiplicar")
+print("4. Dividir")
 
-if a==b==c:
-    print("cambio con fechs y despues pull")  
-else:    
-    print("uno de los 3 numeros son diferentes")
-    
-print("ahi vamos de nuevo")
+opcion = input("Elige una opción (1-4): ")
 
-print("solo se que nada se ")
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
 
+if opcion == "1":
+    resultado = num1 + num2
+    print("Resultado:", resultado)
+
+elif opcion == "2":
+    resultado = num1 - num2
+    print("Resultado:", resultado)
+
+elif opcion == "3":
+    resultado = num1 * num2
+    print("Resultado:", resultado)
+
+elif opcion == "4":
+    if num2 == 0:
+        print("Error: No se puede dividir entre 0")
+    else:
+        resultado = num1 / num2
+        print("Resultado:", resultado)
+
+else:
+    print("Opción no válida")
